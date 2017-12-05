a53b44fa5067e16db2ddeecff5235ca9727d3a51
Jonas A <jonas.ahrens@gmail.com>
2017-12-05 16:02:16 +0100

    Add some checks and comments

--- www/js/board.js
+++ www/js/board.js
@@ -10,11 +10,39 @@ class Board {
             [0, 0, 0, 0, 0, 0]
         ]
     }
+
+    /**
+     * 
+     * 
+     * @param {number} x Horizontal axis
+     * @param {number} y Vertical axis
+     * @returns  0 if empty, 1 for player 1, -1 for player 2, false if invalid slot
+     * @memberof Board
+     */
     getSlot(x, y) {
-        return this.state[x][y]
+        if (x < this.state.length && y < this.state[0].length) {
+            return this.state[x][y]
+        } else {
+            return false
+        }
     }
 
+    /**
+     * Sets a slot to 1 for player 1 and -1 for player 2
+     * 
+     * @param {number} x Horizontal axis
+     * @param {number} y Vertical axis
+     * @param {number} playerId Player 1 or 2
+     * @returns true if valid coordinate
+     * @memberof Board
+     */
     setSlot(x, y, playerId) {
-        this.state[x][y] = playerId == 0 ? 1 : -1
+        try {
+            this.state[x][y] = playerId < 2 ? 1 : -1
+            return true
+        } catch (error) {
+            console.log('Invalid axis')
+            return false
+        }
     }
 }
