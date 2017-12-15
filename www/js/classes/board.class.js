320d288dd5940e230c3134fd64c0fe215d11efb2
Dacrol <jonas.ahrens@gmail.com>
2017-12-15 21:17:36 +0100

    Merge branch 'feature/hover-slots-experimental' into develop (+board.js)

--- www/js/classes/board.class.js
+++ www/js/classes/board.class.js
@@ -1,3 +1,7 @@
+//TODO: Ändra till board.playerX.color överallt
+const player1color = 'red'
+const player2color = 'yellow'
+
 class Board extends Base {
     constructor() {
         super();
@@ -20,6 +24,7 @@ class Board extends Base {
         this.currentPlayer = 1
         this.width = this.state.length
         this.height = this.state[0].length
+        hoverFn(player1color)
     }
 
     /**
@@ -58,9 +63,14 @@ class Board extends Base {
     }
 
     nextPlayer() {
-        this.currentPlayer ^= 3
-            // this.currentPlayer ^= 1 // Switches between 1 and 0 instead
-    }
+      this.currentPlayer ^= 3 // Switches between 1 and 2 // this.currentPlayer ^= 1 // Switches between 1 and 0 instead
+      if(this.currentPlayer === 1){
+        hoverFn(player1color)
+      }
+      if(this.currentPlayer === 2){
+        hoverFn(player2color)
+      }
+  }
 
     placeInColumn(column) {
         let y = this.state[column].findIndex((slot) => {
@@ -88,71 +98,79 @@ class Board extends Base {
 
     }
     checkWinner(bd) {
-        // console.log(bd);
-        // Check down
-        for (let row = 0; row < 6; row++) {
+      // console.log(bd);
+      // Check down
+      for (let row = 0; row < 6; row++) {
 
-            for (let col = 0; col < 7; col++) {
+          for (let col = 0; col < 7; col++) {
 
-                for (let player of[1, -1]) {
+              for (let player of [1, -1]) {
 
-                    //vertical
+                  //vertical
 
+                  // if(bd[col][row] == player){
+                  //   console.log('col', col);
+                  //   console.log('row',row);
+                  // }
 
-                    // if(bd[col][row] == player){
-                    //   console.log('col', col);
-                    //   console.log('row',row);
-                    // }
+                  if (row < 3 && bd[col][row] == player && bd[col][row + 1] == player && bd[col][row + 2] == player && bd[col][row + 3] == player) {
+                      this.winner = player;
+                  }
 
+                  // horizontal
 
-                    if (row < 3 && bd[col][row] == player && bd[col][row + 1] == player && bd[col][row + 2] == player && bd[col][row + 3] == player) {
-                        this.winner = player;
-                    }
+                  if (col < 3 && bd[col][row] == player && bd[col + 1][row] == player && bd[col + 2][row] == player && bd[col + 3][row] == player) {
+                      this.winner = player;
+                  }
 
-                    // horisontel
+                  if (col < 4 && bd[col][row] == player && bd[col + 1][row + 1] == player && bd[col + 2][row + 2] == player && bd[col + 3][row + 3] == player) {
+                      this.winner = player;
+                  }
 
-                    if (col < 3 && bd[col][row] == player && bd[col + 1][row] == player && bd[col + 2][row] == player && bd[col + 3][row] == player) {
-                        this.winner = player;
-                    }
+                  if (col > 2 && row < 3 && bd[col][row] == player && bd[col - 1][row + 1] == player && bd[col - 2][row + 2] == player && bd[col - 3][row + 3] == player) {
+                      this.winner = player;
+                  }
+              }
+          }
+      }
 
+      this.co++;
+      if (this.winner != null) {
+        $('.board-column-hover').css({'opacity': '0'})
+        return true
+      } else if (this.co === 42) {
+          this.winner = 'draw';
+          $('.board-column-hover').css({'opacity': '0'})
+          return true
+      } else {
+        return false
+      }
+  }
 
-                    if (col < 4 && bd[col][row] == player && bd[col + 1][row + 1] == player && bd[col + 2][row + 2] == player && bd[col + 3][row + 3] == player) {
-                        this.winner = player;
-                    }
+    click(element, instances){
+      let parent = element.parent();
+      // if(parent.hasClass('board-column')){
+      //     this.createSlot(parent);
+      // }
 
-                    if (col > 2 && row < 3 && bd[col][row] == player && bd[col - 1][row + 1] == player && bd[col - 2][row + 2] == player && bd[col - 3][row + 3] == player) {
-                        this.winner = player;
-                    }
+      // let e = element
 
+    if (parent.hasClass('board-column-hover') && (this.winner === null)) {
 
-                }
+      let childnumber = parseInt(parent[0].id.split('column-hover-').pop())
+      const row = this.placeInColumn(childnumber)
+      let parentchild = parent[0].parentElement.children[childnumber + 7]
 
-            }
-        }
-
-        this.co++;
-        if (this.co === 42 && this.winner == null) {
-            this.winner = 'draw';
-        }
-
-
-
-
+      this.createSingleSlot($(parentchild), row);
+      this.checkWinner(this.state);
+      } else if (parent.hasClass('board-column') && (this.winner === null)){
+      const row = this.placeInColumn(parent[0].id.split('column-').pop())
+        this.createSingleSlot(parent, row);
+        this.checkWinner(this.state);
+    } else {
+      console.log('Game over');
     }
-    click(element, instances) {
-        let parent = element.parent();
-        // if(parent.hasClass('board-column')){
-        //     this.createSlot(parent);
-        // }
-
-
-        if ((parent.hasClass('board-column') || parent.hasClass('board-column-hover')) && (this.winner === null) ) {
-            this.createSingleSlot(parent);
-            this.checkWinner(this.state);
-        } else {
-          console.log(this.winner);
-        }
-    }
+  }
 
 
     generateBoard() {
@@ -165,57 +183,120 @@ class Board extends Base {
         this.render(parent, '2');
     }
 
-    createSingleSlot(parent) {
-        console.log(parent[0].id.split('column-').pop())
-        const row = this.placeInColumn(parent[0].id.split('column-').pop())
-        if (row != -1) {
-            let element = parent[0].children[this.height - row - 1]
-                // this.render(element, 'single');
-            if (this.currentPlayer == 1) {
-                element.className += ' red'
+    createSingleSlot(parent, row){
+      // console.log(parent[0].id.split('column-').pop())
+      // const row = this.placeInColumn(parent[0].id.split('column-').pop())
+      if (row != -1){
+      let element = parent[0].children[this.height - row - 1]
+      if (this.currentPlayer == 1){
+        element.className += ' red'
 
-            } else if (this.currentPlayer == 2) {
-                element.className += ' yellow'
+      } else if (this.currentPlayer == 2){
+        element.className += ' yellow'
 
-            }
-            console.log(element.className)
-            this.nextPlayer()
-        }
+      }
+      $(parent).hover(function(){
+      })
+      this.nextPlayer()
     }
+  }
 
 
     //need to add id on them as well but here is a start on the board
 
-    template() {
-        let returnValue = '<div class="board col-12">';
-        let inner = '',
-            column = '';
+    template(){
+      let returnValue = '<div class="board col-12">';
+      let inner = '', column = '';
+      let innerhover = '', columnhover = '';
 
-        for (let co = 0; co < 6; co++) {
-            inner += '<div class="board-slot"></div>';
-        };
+      for (let co = 0; co < 1; co++) {
+          innerhover += '<div class="board-slot-hover"></div>';
+      };
 
-        for (let co = 0; co < 7; co++) {
-            column += '<div class="board-column" ' + 'id="column-' + co + '">' + inner + '</div>';
-        };
+      for (let co = 0; co < 7; co++) {
+          column += '<div class="board-column-hover" ' + 'id="column-hover-' + co + '">' + innerhover + '</div>';
+      };
 
-        return returnValue += returnValue + column + '</div>';
+      for (let co = 0; co < 6; co++) {
+          inner += '<div class="board-slot"></div>';
+      };
 
+      for (let co = 0; co < 7; co++) {
+          column += '<div class="board-column" ' + 'id="column-' + co + '">' + inner + '</div>';
+      };
+
+      return returnValue += returnValue + columnhover + column + '</div>';
+
+  }
+
+}
+
+
+
+/**
+ * Hc Svnt Dracones
+ *
+ * @param {string} color
+ */
+function hoverFn(color) {
+  var hoverdiv
+  $('.board-slot-hover').hover(
+    function() {
+      $(this).css({
+        background: color,
+        opacity: '100'
+      })
+      $(this).click(function() {
+        // console.log(((board.currentPlayer == 1) ? player2color: player1color))
+        $(this).css({
+          background: board.currentPlayer == 1 ? player2color : player1color, // TODO: Ändra till board.player2.color och board.player1.color
+          opacity: '100'
+        })
+      })
+    },
+    function() {
+      // $(this).css('background', 'white');
+      $(this).css({
+        background: 'white',
+        opacity: '0'
+      })
     }
-
-    template2() {
-        let wrapper = '<div>';
-        let inner = '';
-        for (let co = 0; co < 6; co++) {
-            inner += '<div class="board-slot red"></div>';
-        };
-        console.log(inner);
-        return inner = wrapper + inner + '</div>';
+  )
+  $('.board-column .board-slot').hover(
+    function() {
+      // $(this).css("background", "red");
+      hoverdiv = $(this)
+        .parent()
+        .parent()
+        .children(
+          '#column-hover-' +
+            $(this)
+              .parent()
+              .attr('id')
+              .split('column-')
+              .pop()
+        )
+        .children('.board-slot-hover')
+      $(this).click(() => {
+        // console.log(((board.currentPlayer == 1) ? player2color: player1color))
+        hoverdiv.css({
+          background: board.currentPlayer == 1 ? player2color : player1color, // TODO: Ändra till board.player2.color och board.player1.color
+          // opacity: '100'
+        })
+      })
+      hoverdiv.css({
+        background: color,
+        // opacity: '100'
+      })
+    },
+    function() {
+      // $(this).css('background', 'white');
+      if (hoverdiv) {
+        hoverdiv.css({
+          background: 'white',
+          // opacity: '0'
+        })
+      }
     }
-
-    templatesingle() {
-        return `<div class="board-slot red"></div>`;
-    }
-
-
+  )
 }
