acd72fbad030e959dbb214b36b81e0e46001d3ad
Jonas A <jonas.ahrens@gmail.com>
2017-12-19 21:03:28 +0100

    Add many error checks
    So many

--- www/js/classes/game.class.js
+++ www/js/classes/game.class.js
@@ -1,21 +1,23 @@
 class Game {
     constructor() {
-      this.getPlayers();
+      // this.getPlayers();
+      if (this.getPlayers() && this.player1 && this.player2){ //Ready to start game
+        this.newGame();
+      }
     }
 
     newGame() {
     	this.loadHiScores();
-        this.board = new Board(this);
-        this.board.generateBoard();
-        //return this.board; // really?
+      this.board = new Board(this);
+      this.board.generateBoard();
     }
 
-    setName(player1, player2) {
-        let name1 = $('#playerName1').val();
-        let name2 = $('#playerName2').val();
-        name1 = this.player1;
-        name2 = this.player2;
-    }
+    // setName(player1, player2) {
+    //     let name1 = $('#playerName1').val();
+    //     let name2 = $('#playerName2').val();
+    //     name1 = this.player1.name;
+    //     name2 = this.player2.name;
+    // }
 
 
     setPlayer1Type(response) {
@@ -27,19 +29,29 @@ class Game {
     }
 
     getPlayers() {
-    	if(loadPlayers()){
-    		let players = loadPlayers();
-    		this.player1 = players[0];
-    		this.player2 = players[1];
-        this.board = this.newGame(this.player1, this.player2);
-    	}
-    	
+      let players = loadPlayers()
+    	if (Array.isArray(players)){
+        try {
+          this.player1 = players[0];
+    	  	this.player2 = players[1];
+          // this.board = this.newGame(); //Moved to line 4
+          return true;
+        } catch (error) {
+          console.warn('Failed to load players from localStorage:' + error.message)
+          return false // We are on play.html but we could not load players
+        }
+      } else {
+        return false; // We're not on play.html
+      }
+
     }
 
 
     loadHiScores(){
-        JSON._classes(HighscoreList, Human, Bot);
-        JSON._load('hi-scores')
+      // @ts-ignore
+      JSON._classes(HighscoreList, Human, Bot);
+      // @ts-ignore
+      JSON._load('hi-scores')
         .then((data) => {
             this.highscoreList = data.list;
         })
@@ -99,32 +111,34 @@ let url = location.pathname;
 
 function loadPlayers() {
     //Default values for player type is human, only if page is not play.html
-    if (!(url == '/play.html')) {
+    if (!(url == '/play.html' || url == '/player.html')) {
         localStorage.setItem('player1Type', "human");
         localStorage.setItem('player2Type', "human");
-    }
-
+        return false; // Not ready to start
+    } else if (url == '/player.html') {
     $(document).on('click', '.startGame', function() {
-        let val1 = $('#playerName1').val();
-        let val2 = $('#playerName2').val();
-        localStorage.setItem('player1Name', val1);
-        localStorage.setItem('player2Name', val2);
+        let val1 = $('#playerName1').val() || 'Player 1';
+        let val2 = $('#playerName2').val() || 'Player 2';
+        localStorage.setItem('player1Name', val1.toString());
+        localStorage.setItem('player2Name', val2.toString());
+
     });
+    return false; // Still not ready to start
+    } else if (url == "/play.html") {
     //If play.html, get player names and types and render to page
-    if (url == "/play.html") {
-        let name1 = localStorage.getItem('player1Name');
-        let name2 = localStorage.getItem('player2Name');
-        let type1 = localStorage.getItem('player1Type');
-        let type2 = localStorage.getItem('player2Type');
-        console.log(name1 + " is type " + type1);
-        console.log(name2 + " is type " + type2);
-        $('.player1').html(name1);
-        $('.player2').html(name2);
+    let name1 = localStorage.getItem('player1Name') || 'Player 1';
+    let name2 = localStorage.getItem('player2Name') || 'Player 2';
+    let type1 = localStorage.getItem('player1Type') || 'human';
+    let type2 = localStorage.getItem('player2Type') || 'human';
+    console.log(name1 + " is type " + type1);
+    console.log(name2 + " is type " + type2);
+    $('.player1').html(name1);
+    $('.player2').html(name2);
 
-        let player1 = type1 == 'human' ? new Human(name1) : new Bot(name1);
-        let player2 = type2 == 'human' ? new Human(name2) : new Bot(name2);
+    let player1 = type1 == 'human' ? new Human(name1) : new Bot(name1);
+    let player2 = type2 == 'human' ? new Human(name2) : new Bot(name2);
 
-        return [player1, player2];
+    return [player1, player2]; //Ready to start
 
     }
 }
