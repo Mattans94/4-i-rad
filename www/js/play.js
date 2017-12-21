4c8bb0ea0e0a9ec1d90fbca1b7d690ffa0c71e9b
Malin Rasmusson <info@formidable.se>
2017-12-21 16:09:21 +0100

    load player name and type

--- www/js/play.js
+++ www/js/play.js
@@ -3,3 +3,11 @@ $('#gameOveModal').on('shown.bs.modal', function (event) {
   //modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body').text(" ");
 })
+
+$('#player1name').text(game.board.player1.name + ' ');
+$('#player1type')[0].innerHTML = ' ' + (game.board.player1.constructor.name == 'Human' ? `&#x1F64B;` : `&#x1F4BB;`);
+$('#player1moves').text(0);
+
+$('#player2name').text(game.board.player2.name + ' ');
+$('#player2type')[0].innerHTML = ' ' + (game.board.player2.constructor.name == 'Human' ? `&#x1F64B;` : `&#x1F4BB;`);
+$('#player2moves').text(0);
